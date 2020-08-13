34660d57506118ba25789a2b76ef13aef79de9ab
Nicolás Fernández <Nico.E.Fernandez@hotmail.com>
2020-08-13 11:02:06 -0300

    End of section 8

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -31,7 +31,7 @@ namespace DatingApp.API.Controllers
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
-            {
+            {   
                 return BadRequest("Username already exists");
             }
 
@@ -49,9 +49,9 @@ namespace DatingApp.API.Controllers
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {  
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
-
+            
             if (userFromRepo == null)
-            {
+            {                
                 return Unauthorized();
             }
 
